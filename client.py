d420b887b910c51af79f3c359fa900102e6c3b9d
Mauricio Verano Merino <m.verano239@gmail.com>
2021-05-06 12:30:13 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ from cffi import FFI
 
 ffi = FFI()
 ffi.cdef("""
-        int fooz();
+        int fooz(int a);
 """)
 
 # lib = ffi.dlopen("Dotnet_API.dylib")
 lib = ffi.dlopen("lang_protocol.dylib")
 print("lib loaded")
 
-print(lib.fooz())
+print(lib.fooz(7))
