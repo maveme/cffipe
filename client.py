853d834568c858f70b12935558d6b2725c92f839
Mauricio Verano Merino <m.verano239@gmail.com>
2021-05-06 13:57:04 +0200

    cleaned up the client

--- client.py
+++ client.py
@@ -1,20 +1,14 @@
 from cffi import FFI
 
-# import os
-
 ffi = FFI()
 ffi.cdef("""
         int fooz(int a);
 """)
 
 # lib = ffi.dlopen("Dotnet_API.dylib")
-# print(os.environ)
 def call():
-    # lib = ffi.dlopen("lang_protocol.dylib")
-    # lib = ffi.dlopen("/Users/mauricio/Documents/ResearchProjects/Siemens/testing/lang_protocol.dylib")
     lib = ffi.dlopen("/lang_protocol.so")
     print("lib loaded")
-    # num = ffi.new("int *", 6)
     print(lib.fooz(6))
     print("finished")
     ffi.dlclose(lib)
